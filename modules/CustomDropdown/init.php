9d599332008b6bc23e2b7b2870e2f9d1c204a5eb
GIGABAIT93 <freebitcoin93@gmail.com>
2022-06-11 20:08:57 +0300

    update pre13

--- modules/CustomDropdown/init.php
+++ modules/CustomDropdown/init.php
@@ -1,24 +1,15 @@
 <?php
-/*
- *  Made by Samerton
- *  https://github.com/NamelessMC/Nameless/tree/v2/
- *  NamelessMC version 2.0.0-pr9
- *
- *  License: MIT
- *
- *  CustomDropdown By xGIGABAITx
- */
 
 $INFO_MODULE = array(
-	'name' => 'CustomDropdown',
-	'author' => '<a href="https://tensa.co.ua" target="_blank" rel="nofollow noopener">xGIGABAITx</a>',
-	'module_ver' => '1.2.1',
-	'nml_ver' => '2.0.0-pr12',
+  'name' => 'CustomDropdown',
+  'author' => '<a href="https://tensa.co.ua" target="_blank" rel="nofollow noopener">xGIGABAITx</a>',
+  'module_ver' => '1.2.2',
+  'nml_ver' => '2.0.0-pr13',
 );
 
-$CustomDropdownLanguage = new Language(ROOT_PATH . '/modules/' . $INFO_MODULE['name'] . '/language', LANGUAGE);
+$csLanguage = new Language(ROOT_PATH . '/modules/' . $INFO_MODULE['name'] . '/language', LANGUAGE);
 
-$GLOBALS['CustomDropdownLanguage'] = $CustomDropdownLanguage;
+$GLOBALS['csLanguage'] = $csLanguage;
 
 require_once(ROOT_PATH . '/modules/' . $INFO_MODULE['name'] . '/module.php');
 
--- modules/CustomDropdown/module.php
+++ modules/CustomDropdown/module.php
@@ -1,152 +1,142 @@
 <?php
-/*
- *  Made by Samerton
- *  https://github.com/NamelessMC/Nameless/tree/v2/
- *  NamelessMC version 2.0.0-pr12
- *
- *  License: MIT
- *
- *  CustomDropdown By xGIGABAITx
- */
 
 class CustomDropdown_Module extends Module
 {
 
-	private $_language, $CustomDropdownLanguage;
+  private $_language, $csLanguage;
 
-	public function __construct($language, $pages, $INFO_MODULE)
-	{
-		$this->_language = $language;
+  public function __construct($language, $pages, $INFO_MODULE)
+  {
+    $this->_language = $language;
 
-		$this->CustomDropdownLanguage = $GLOBALS['CustomDropdownLanguage'];
+    $this->csLanguage = $GLOBALS['csLanguage'];
 
-		$name = $INFO_MODULE['name'];
-		$author = $INFO_MODULE['author'];
-		$module_version = $INFO_MODULE['module_ver'];
-		$nameless_version = $INFO_MODULE['nml_ver'];
-		parent::__construct($this, $name, $author, $module_version, $nameless_version);
+    $name = $INFO_MODULE['name'];
+    $author = $INFO_MODULE['author'];
+    $module_version = $INFO_MODULE['module_ver'];
+    $nameless_version = $INFO_MODULE['nml_ver'];
+    parent::__construct($this, $name, $author, $module_version, $nameless_version);
 
-		$pages->add('CustomDropdown', '/panel/cs-dropdown', 'pages/panel/index.php');
+    $pages->add('CustomDropdown', '/panel/cs-dropdown', 'pages/panel/index.php');
+  }
 
-	}
+  public function onInstall()
+  {
+    $this->initialise();
+  }
 
-	public function onInstall()
-	{
-		$this->initialise();
-	}
+  public function onUninstall()
+  {
+  }
 
-	public function onUninstall()
-	{
-	}
+  public function onEnable()
+  {
+    $this->initialise();
+  }
 
-	public function onEnable()
-	{
-		$this->initialise();
-	}
+  public function onDisable()
+  {
+  }
 
-	public function onDisable()
-	{
-	}
+  public function onPageLoad($user, $pages, $cache, $smarty, $navs, $widgets, $template)
+  {
 
-	public function onPageLoad($user, $pages, $cache, $smarty, $navs, $widgets, $template)
-	{
+    PermissionHandler::registerPermissions('CustomDropdown', array(
+      'admincp.csdropdown' => $this->csLanguage->get('general', 'group_permision')
+    ));
 
-		PermissionHandler::registerPermissions('CustomDropdown', array(
-			'admincp.csdropdown' => $this->CustomDropdownLanguage->get('general', 'group_permision')
-		));
+    if (defined('FRONT_END')) {
 
-		if (defined('FRONT_END')) {
-
-			$queries = new Queries();
-
-
-			$dropdown_list = $queries->getWhere('custom_dropdown', array('id', '<>', 0));
-			if (count($dropdown_list)) {
-
-				foreach ($dropdown_list as $var) {
-
-					// Add CustomDropdown
-					$name = Output::getClean($var->id);
-					$title = Output::getClean($var->dropdown_title);
-					$location_var = $var->dropdown_location;
-					$icon = $var->dropdown_icon;
-					$order = $var->dropdown_order;
-
-					if ($var->enabled == 0) {
-						continue;
-					}
-
-					if ($location_var == 2) {
-						$location = 'footer';
-					} else {
-						$location = 'top';
-					}
-
-					$navs[0]->addDropdown($name, $title, $location, $order, $icon);
-				}
-			}
-
-			$dropdown_pages_list = $queries->getWhere('custom_dropdown_pages', array('id', '<>', 0));
-			if (count($dropdown_pages_list)) {
-
-				foreach ($dropdown_pages_list as $var) {
-
-					// Add "More" dropdown pages
-					$dropdown = Output::getClean($var->dropdown_id);
-					$name = Output::getClean($var->id);
-					$title = Output::getClean($var->page_title);
-					$link = URL::build($var->page_link);
-					$location = $var->page_location;
-					$target = $var->page_target;
-					$icon = (string) $var->page_icon;
-					$order = $var->page_order;
-
-					$navs[0]->addItemToDropdown($dropdown, $name, $title, $link, $location, $target, $icon, $order);
-				}
-			}
-		} else if (defined('BACK_END')) {
-
-			$icon = '<i class="nav-icon fas fa-caret-square-down"></i>';
-
-			$cs_dropdown_title = $this->CustomDropdownLanguage->get('general', 'title');
+      $queries = new Queries();
 
 
-			if ($user->hasPermission('admincp.csdropdown')) {
+      $dropdown_list = $queries->getWhere('custom_dropdown', array('id', '<>', 0));
+      if (count($dropdown_list)) {
 
-				$order = 33;
+        foreach ($dropdown_list as $var) {
 
-				$navs[2]->add('cs_dropdown_divider', mb_strtoupper($cs_dropdown_title, 'UTF-8'), 'divider', 'top', null, $order, '');
+          // Add CustomDropdown
+          $name = Output::getClean($var->id);
+          $title = Output::getClean($var->dropdown_title);
+          $location_var = $var->dropdown_location;
+          $icon = $var->dropdown_icon;
+          $order = $var->dropdown_order;
 
-				$navs[2]->add('cs_dropdown_items', $cs_dropdown_title, URL::build('/panel/cs-dropdown'), 'top', null, $order + 0.1, $icon);
-			}
-		}
-	}
+          if ($var->enabled == 0) {
+            continue;
+          }
 
-	public function initialise()
-	{
-		$queries = new Queries();
+          if ($location_var == 2) {
+            $location = 'footer';
+          } else {
+            $location = 'top';
+          }
 
-		try {
+          $navs[0]->addDropdown($name, $title, $location, $order, $icon);
+        }
+      }
 
-			$data = $queries->createTable("custom_dropdown", " `id` int(11) NOT NULL AUTO_INCREMENT, `dropdown_title` varchar(255) NOT NULL, `dropdown_location` int(1) NOT NULL, `dropdown_icon` varchar(255) NOT NULL, `dropdown_order` int(11) NOT NULL, `enabled` int(1) NOT NULL DEFAULT '1', PRIMARY KEY (`id`)", "ENGINE=InnoDB DEFAULT CHARSET=utf8");
+      $dropdown_pages_list = $queries->getWhere('custom_dropdown_pages', array('id', '<>', 0));
+      if (count($dropdown_pages_list)) {
 
-			$data = $queries->createTable("custom_dropdown_pages", " `id` int(11) NOT NULL AUTO_INCREMENT, `dropdown_id` int(11) NOT NULL, `page_title` varchar(255) NOT NULL, `page_link` varchar(512) NOT NULL, `page_location` varchar(255) NOT NULL DEFAULT 'top', `page_target` varchar(20) DEFAULT NULL, `page_icon` varchar(255) DEFAULT NULL, `page_order` int(11) DEFAULT '0', `default_page_id` int(11) NOT NULL DEFAULT '0', PRIMARY KEY (`id`)", "ENGINE=InnoDB DEFAULT CHARSET=utf8");
-		} catch (Exception $e) {
-			echo $e->getMessage();
-		}
+        foreach ($dropdown_pages_list as $var) {
 
-		try {
+          // Add "More" dropdown pages
+          $dropdown = Output::getClean($var->dropdown_id);
+          $name = Output::getClean($var->id);
+          $title = Output::getClean($var->page_title);
+          $link = URL::build($var->page_link);
+          $location = $var->page_location;
+          $target = $var->page_target;
+          $icon = (string) $var->page_icon;
+          $order = $var->page_order;
 
-			$group = $queries->getWhere('groups', array('id', '=', 2));
-			$group = $group[0];
+          $navs[0]->addItemToDropdown($dropdown, $name, $title, $link, $location, $target, $icon, $order);
+        }
+      }
+    } else if (defined('BACK_END')) {
 
-			$group_permissions = json_decode($group->permissions, TRUE);
-			$group_permissions['admincp.csdropdown'] = 1;
+      $icon = '<i class="nav-icon fas fa-caret-square-down"></i>';
+      $cs_dropdown_title = $this->csLanguage->get('general', 'title');
 
-			$group_permissions = json_encode($group_permissions);
-			$queries->update('groups', 2, array('permissions' => $group_permissions));
-		} catch (Exception $e) {
-			echo $e->getMessage();
-		}
-	}
+
+      if ($user->hasPermission('admincp.csdropdown')) {
+        $order = end($navs[2]->returnNav())['order'] + 2.2;
+        $navs[2]->add('cs_dropdown_divider', mb_strtoupper($cs_dropdown_title, 'UTF-8'), 'divider', 'top', null, $order, '');
+        $navs[2]->add('cs_dropdown_items', $cs_dropdown_title, URL::build('/panel/cs-dropdown'), 'top', null, $order + 0.1, $icon);
+      }
+    }
+  }
+
+  public function initialise()
+  {
+    $queries = new Queries();
+
+    try {
+
+      $queries->createTable("custom_dropdown", " `id` int(11) NOT NULL AUTO_INCREMENT, `dropdown_title` varchar(255) NOT NULL, `dropdown_location` int(1) NOT NULL, `dropdown_icon` varchar(255) NOT NULL, `dropdown_order` int(11) NOT NULL, `enabled` int(1) NOT NULL DEFAULT '1', PRIMARY KEY (`id`)", "ENGINE=InnoDB DEFAULT CHARSET=utf8");
+
+      $queries->createTable("custom_dropdown_pages", " `id` int(11) NOT NULL AUTO_INCREMENT, `dropdown_id` int(11) NOT NULL, `page_title` varchar(255) NOT NULL, `page_link` varchar(512) NOT NULL, `page_location` varchar(255) NOT NULL DEFAULT 'top', `page_target` varchar(20) DEFAULT NULL, `page_icon` varchar(255) DEFAULT NULL, `page_order` int(11) DEFAULT '0', `default_page_id` int(11) NOT NULL DEFAULT '0', PRIMARY KEY (`id`)", "ENGINE=InnoDB DEFAULT CHARSET=utf8");
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+
+    try {
+
+      $group = $queries->getWhere('groups', array('id', '=', 2));
+      $group = $group[0];
+
+      $group_permissions = json_decode($group->permissions, TRUE);
+      $group_permissions['admincp.csdropdown'] = 1;
+
+      $group_permissions = json_encode($group_permissions);
+      $queries->update('groups', 2, array('permissions' => $group_permissions));
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
+
+  public function getDebugInfo(): array {
+    return [];
+  }
 }
