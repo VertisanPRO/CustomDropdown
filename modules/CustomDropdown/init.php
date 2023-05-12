dcfea821e6aaf3214aafab44602d03c03f7e74fa
PadowYT2 <me@padow.ru>
2023-05-12 15:45:44 +0300

    Last update
    NamelessMC 2.2.0 will have the module included (I'll try to make it)

--- modules/CustomDropdown/init.php
+++ modules/CustomDropdown/init.php
@@ -4,7 +4,7 @@ $INFO_MODULE = [
     'name' => 'CustomDropdown',
     'author' => '<a href="https://github.com/VertisanPRO" target="_blank" rel="nofollow noopener">Vertisan</a>',
     'module_ver' => '1.2.4',
-    'nml_ver' => '2.0.1',
+    'nml_ver' => '2.1.0',
 ];
 
 $csLanguage = new Language(ROOT_PATH . '/modules/' . $INFO_MODULE['name'] . '/language', LANGUAGE);
